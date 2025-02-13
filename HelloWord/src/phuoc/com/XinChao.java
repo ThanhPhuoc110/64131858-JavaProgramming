049ea9f9d88f55c31c5d7389de5e9ae9a0dc6640
ThanhPhuoc110 <thanhphuoc26062003@gmail.com>
2025-02-14 00:50:36 +0700

    Update XinChao.java

--- HelloWord/src/phuoc/com/XinChao.java
+++ HelloWord/src/phuoc/com/XinChao.java
@@ -4,6 +4,8 @@ public class XinChao {
 
 	public static void main(String[] args) {
 		System.out.println("Tôi lần đầu học Java, và đang thử nghiệm quản lý mã nguồn với GIT");
+		System.out.println("Tôi thêm 1 dòng mới, và sửa lại chữ Java ở trên");
+
 	}
 
 }
