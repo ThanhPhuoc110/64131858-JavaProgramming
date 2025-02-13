2c9f882063ed270701c932c5f8dd08a868b09fdd
ThanhPhuoc110 <thanhphuoc26062003@gmail.com>
2025-02-14 00:33:37 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ src/phuoc/com/XinChao.java
@@ -0,0 +1,9 @@
+package phuoc.com;
+
+public class XinChao {
+
+	public static void main(String[] args) {
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, VÀ ĐANG THỬ NGHIỆM QUẢN LÝ MÃ NGUỒN VỚI GIT");
+
+}
+	}
